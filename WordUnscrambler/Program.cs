491d0190b0b9e982b7e74b987da85d1654d6a225
Firefly4266 <gaines.johnp@gmail.com>
2017-10-01 23:23:44 -0700

    Skeleton, no methods yet...just dummies.

--- WordUnscrambler/Program.cs
+++ WordUnscrambler/Program.cs
@@ -10,6 +10,49 @@ namespace WordUnscrambler
     {
         static void Main(string[] args)
         {
+            bool loopCondition = true;
+            do
+            {
+                Console.Write("\n Please select -- F for File or M for Manual: ");
+                var option = Console.ReadLine() ?? string.Empty;
+
+                switch (option.ToUpper())
+                {
+                    case "F":
+                        Console.Write("\n Enter scrambled words file name: ");
+                        MethodToProcessFile();
+                        break;
+                    case "M":
+                        Console.Write("\n Enter scrambled words: ");
+                        MethodToProcessWords();
+                        break;
+                    default:
+                        Console.WriteLine("\n Sorry, not recoginized");
+                        break;
+                }
+
+                var getUserAnswer = string.Empty;
+                do
+                {
+                    Console.Write("\n Would you like to continue? Y/N ");
+                    getUserAnswer = (Console.ReadLine() ?? string.Empty);
+
+                } while (
+                    !getUserAnswer.Equals("Y", StringComparison.OrdinalIgnoreCase) && 
+                    !getUserAnswer.Equals("N", StringComparison.OrdinalIgnoreCase));
+                    loopCondition = getUserAnswer.Equals("Y", StringComparison.OrdinalIgnoreCase);
+            } while (loopCondition);
+
+        }
+
+        private static void MethodToProcessWords()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void MethodToProcessFile()
+        {
+            throw new NotImplementedException();
         }
     }
 }
