395412cf5f881bb3d456e2ba33c002b9b6ba3270
Firefly4266 <gaines.johnp@gmail.com>
2017-10-02 22:17:38 -0700

    Completed all methods

--- WordUnscrambler/Constructors/WordMatcher.cs
+++ WordUnscrambler/Constructors/WordMatcher.cs
@@ -9,9 +9,45 @@ namespace WordUnscrambler.Constructors
 {
     class WordMatcher
     {
-        internal List<MatchedWord> Match(string[] scrambledWords, string[] wordList)
+        public List<MatchedWord> Match(string[] scrambledWords, string[] wordList)
         {
-            throw new NotImplementedException();
+            var matchedWords = new List<MatchedWord>();
+            foreach (var scramWord in scrambledWords)
+            {
+                foreach (var word in wordList)
+                {
+                    if (scramWord.Equals(word, StringComparison.OrdinalIgnoreCase))
+                    {
+                        matchedWords.Add(BuildMatchedWord(scramWord, word));
+                    }
+                    else
+                    {
+                        var scramWordArray = scramWord.ToCharArray();
+                        var wordArray = word.ToCharArray();
+
+                        Array.Sort(scramWordArray);
+                        Array.Sort(wordArray);
+
+                        var sortedScram = scramWordArray.ToString();
+                        var sortedWord = wordArray.ToString();
+                        if (sortedScram.Equals(sortedWord, StringComparison.OrdinalIgnoreCase))
+                        {
+                            matchedWords.Add(BuildMatchedWord(scramWord, word));
+                        }
+                    }
+                }
+            }
+
+            return matchedWords;
+        }
+        private MatchedWord BuildMatchedWord(string scramWord, string word)
+        {
+            MatchedWord matchedWord = new MatchedWord
+            {
+                ScrambledWords = scramWord,
+                Word = word
+            };
+            return matchedWord;
         }
     }
 }
